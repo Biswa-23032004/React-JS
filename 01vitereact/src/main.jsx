689ad62991e3396dc6692f0c7d10a82d920423fd
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-07-24 22:14:35 +0530

    basic react app with vite

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {jsx as _jsx} from "react/jsx-runtime.js"
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App | Maggie</h1>
+        </div>
+    )
+}
+
+// const reactElement ={
+//     type: 'a',
+//     props: {
+//         herf: 'https://gmail.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit Gmail'
+// }
+
+const anotherElement = (
+    <a href="https://gmail.com" target='_blank'>Gmail</a>
+)
+
+
+const anotherUser = "Maggie in 2 minute"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://gmail.com',target:'_blank' },
+    'click me to visit Gmail',
+    anotherElement
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    reactElement
   
 )
