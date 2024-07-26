62b6d2d7a38ae3caf6f08931f25d7a966109a4c8
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-07-26 21:47:19 +0530

    ract hooks and projects

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Maggie aur soup</h1>
+    <h2>Counter value: 5</h2>
+
+    <button>Add value</button>
+    <br />
+    <button>Remove value</button>
+    </>
+  )
+}
+
+export default App
