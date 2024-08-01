53ad1e2dca2e0c9a874c601cd97a890176d194e3
Biswa-23032004 <nayakbiswajit292@gmail.com>
2024-08-01 22:40:27 +0530

    ract hooks and projects

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,18 +5,50 @@ import './App.css'
 
 function App() {
 
-  let [counter,setCounter] = useState(15)
+      // Let us add a onclick() in the button to increase or decrease the counter value
+    // After clicking the buttons the Ui doesnot change but we can see the changes in console.
+    // To make it possible here comes REACT hooks
+
+    // React Hooks are functions that allow you to use state and other React features in functional components without writing a class.
+    //They were introduced in React 16.8 as a way to enable stateful logic and side effects in functional components, which were previously only possible with class components.
+
+
+    // Here we are going to use a react hook that is {useState}
+    // To use we have to import it = import { useState } from 'react'
+
+  let [counter,setCounter] = useState(1)
 
   // let counter = 15
 
   const addValue = () => {
-    console.log("clicked", counter);
-    counter = counter + 1
 
-    setCounter(counter)
-    // setCounter(counter + 1)
-
+    if (counter < 20) {
+      setCounter(counter + 1)
+    }else {
+      setCounter(counter)
+    }
   }
+
+  const removevalue = () => {
+    if(counter >= 0){
+      setCounter(0)
+    } else {
+      setCounter(counter - 1)
+    }
+  }
+
+  //   console.log("clicked", counter);
+  //   counter = counter + 1
+
+  //   setCounter(counter)
+
+  //   // setCounter(counter + 1)
+
+  // }
+
+  // const removevalue = () => {
+  //   setCounter(counter - 1)
+  // }
 
   return (
     <>
@@ -25,7 +57,7 @@ function App() {
 
     <button onClick={addvalue}>Add value {counter}</button>
     <br />
-    <button>Remove value {counter}</button>
+    <button onClick={removevalue}>Remove value {counter}</button>
     <p>Footer: {counter}</p>
     </>
   )
